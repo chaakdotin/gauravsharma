b95284b66b0f1ba7c77cdb0e277fd135f01cc3e7
chaakdotin <admin@chaak.in>
2025-08-11 19:25:16 +0530

    sdfsdfdf

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ import './assets/css/App.css'
 import Header from './header'
 import Home from './home'
 import Work1 from './work1'
+import Work1 from './work1'
+import Work2 from './work1'
+import Work3 from './work1'
+import Work4 from './work1'
+import Work5 from './work1'
+import Work6 from './work1'
+import Work7 from './work1'
+import Work8 from './work1'
 import Motionmotion from './motionmotion'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 function App() {
@@ -17,6 +25,13 @@ function App() {
       <Routes>
         <Route path="/" element={<Work1 />} />
         <Route path="/work" element={<Work1 />} />
+        <Route path="/work" element={<Work2 />} />
+        <Route path="/work" element={<Work3 />} />
+        <Route path="/work" element={<Work4 />} />
+        <Route path="/work" element={<Work5 />} />
+        <Route path="/work" element={<Work6 />} />
+        <Route path="/work" element={<Work7 />} />
+        <Route path="/work" element={<Work8 />} />
         <Route path="/work/motionmotion" element={<Motionmotion />} />
       </Routes>
     </Router>
